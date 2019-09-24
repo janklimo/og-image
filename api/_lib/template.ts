346321f0c8a1ca5ec7ac30a1adf266cd36e60f78
Jan Klimo <jan.klimo@gmail.com>
2019-09-24 21:12:36 +0700

    Applied preview

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,10 +1,4 @@
 import { readFileSync } from "fs";
-import marked from "marked";
-import { sanitizeHtml } from "./sanitizer";
-import { ParsedRequest } from "./types";
-const twemoji = require("twemoji");
-const twOptions = { folder: "svg", ext: ".svg" };
-const emojify = (text: string) => twemoji.parse(text, twOptions);
 
 const regular = readFileSync(
   `${__dirname}/../_fonts/pt-sans-regular.woff2`
@@ -16,13 +10,11 @@ const bold = readFileSync(`${__dirname}/../_fonts/pt-sans-bold.woff2`).toString(
 const avatar = readFileSync(`${__dirname}/../_images/avatar.jpg`).toString(
   "base64"
 );
+const tada = readFileSync(`${__dirname}/../_images/tada.png`).toString(
+  "base64"
+);
 
-function getCss(theme: string, fontSize: string) {
-  let foreground = "black";
-
-  if (theme === "dark") {
-    foreground = "white";
-  }
+function getCss() {
   return `
     @font-face {
         font-family: 'PT Sans';
@@ -39,80 +31,104 @@ function getCss(theme: string, fontSize: string) {
     }
 
     body {
-        background: #fefefe;
-        height: 100vh;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
+      font-family: "PT Sans", sans-serif;
+      background: #fefefe;
+      height: 100vh;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
     }
 
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
+    .avatar-wrapper {
+      display: flex;
+      align-items: center;
+      align-content: center;
+      justify-content: center;
+      justify-items: center;
     }
 
-    .logo {
+    .avatar {
       border-radius: 50%;
-      margin: 1rem auto;
+      margin: 2rem auto;
     }
 
-    .spacer {
-        margin: 150px;
+    .wrapper {
+      margin: 150px 250px;
+      width: 100%;
     }
 
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
-    
     .heading {
-        font-family: 'PT Sans', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
+      font-size: 100px;
+      font-weight: 700;
+      text-align: left;
+      color: #424242;
+      line-height: 1.8;
+    }
+
+    .site-title-wrapper {
+      margin-bottom: 65px;
+    }
+
+    .site-title {
+      font-size: 55px;
+      font-weight: 700;
+      margin: 0;
+      text-transform: uppercase;
+      background: #38c2f0;
+      color: #ffffff;
+      padding: 11px 18px;
+    }
+
+    .new-blog-post {
+      color: #aeadad;
+      font-size: 40px;
     }`;
 }
 
-export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, md, fontSize } = parsedReq;
+export function getHtml() {
   return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+      ${getCss()}
     </style>
     <body>
-        <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-              ${getImage()}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-              md ? marked(text) : sanitizeHtml(text)
-            )}
-            </div>
-            <p>🎉</p>
+      <div class="wrapper">
+        <div class="site-title-wrapper">
+          <span class="site-title">Klimo.io</span>
         </div>
+        <div class="avatar-wrapper">
+          ${getImage()}
+        </div>
+        <p class="new-blog-post">
+          New blog post
+          ${getEmoji()}
+        </p>
+        <div class="heading">
+          My blog post title
+        </div>
+      </div>
     </body>
 </html>`;
 }
 
-function getImage(width = "auto", height = "380") {
+function getImage() {
   return `<img
-        class="logo"
+        class="avatar"
         alt="Generated Image"
         src="data:image/jpeg;charset=utf-8;base64,${avatar}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
+        width="350"
+    />`;
+}
+
+function getEmoji() {
+  return `<img
+        class="tada"
+        alt="Party popper"
+        src="data:image/png;charset=utf-8;base64,${tada}"
+        width="30"
     />`;
 }
